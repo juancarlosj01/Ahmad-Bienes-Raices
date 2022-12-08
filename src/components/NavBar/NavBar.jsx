2d029ec0dcdc060ca88a81a9e63526f7a78f0cbd
Jonnad89 <Jonnad89>
2022-12-08 18:01:00 -0300

    navBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NavBar.css'
+const NavBar = () => {
+  return (
+    <div className='navBar'>
+      <div className='list'>
+        <ul>
+          <li id='inicio'>Inicio</li>
+          <li>Agentes</li>
+          <li>Contacto</li>
+          <li>Reserva online</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
+
+// https://github.com/juancarlosj01/Ahmad-Bienes-Raices.git
