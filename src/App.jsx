92403805dfca87a88f1bd029d2523d7c4c398469
Jonnad89 <Jonnad89>
2022-12-08 18:29:35 -0300

    principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import Principal from './components/Principal/Principal'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
    <NavBar />
+   <Principal />
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './NavBar.css'
+import fulldev from '../../assets/fulldevlatam2.png'
 const NavBar = () => {
   return (
     <div className='navBar'>
       <div className='list'>
+       {/* <div id='logo'> <img src={fulldev} alt="" /></div> */}
         <ul>
           <li id='inicio'>Inicio</li>
           <li>Agentes</li>
           <li>Contacto</li>
           <li>Reserva online</li>
         </ul>
+        
       </div>
     </div>
   )
